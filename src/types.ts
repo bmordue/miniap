b87f68f86b0cb61aa8643afa977154e319ca6c7d
Ben Mordue <benmordue@gmail.com>
2024-11-01 14:04:24 +0000

    export types

---
+++ src/types.ts
@@ -0,0 +1,43 @@
+export type Actor = {
+  "@context": string[];
+  type: string;
+  id: string;
+  inbox: string;
+  outbox: string;
+  following: string;
+  followers: string;
+  preferredUsername: string;
+  name: string;
+  publicKey?: {
+    id: string;
+    owner: string;
+    publicKeyPem: string;
+  };
+}
+
+export type OrderedCollection = {
+  "@context": string;
+  type: string;
+  totalItems: number;
+  orderedItems: any[];
+}
+
+export type Note = {
+  "@context": string;
+  type: string;
+  id: string;
+  attributedTo: string;
+  content: string;
+  published: string;
+  to: string[];
+}
+
+export type Create = {
+  "@context": string;
+  type: string;
+  id: string;
+  actor: string;
+  published: string;
+  object: Note;
+  to: string[];
+}
